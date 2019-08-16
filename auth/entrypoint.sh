8dcd81da6240030896495a5b9180e5f8f8eab6bb
Swen Mun <longfinfunnel@gmail.com>
2019-08-16 17:07:45 +0900

    ffffixup

--- auth/entrypoint.sh
+++ auth/entrypoint.sh
@@ -3,7 +3,7 @@
 set -e
 
 echo "$GCLOUD_AUTH" | base64 --decode > "$HOME"/gcloud.json
-echo "$GCLOUD_AUTH" | wc -l
-echo "$GCLOUD_AUTH" | base64 --decode | wc -l
-<"$HOME"/gcloud.json | wc -l
+echo "$GCLOUD_AUTH" | wc
+echo "$GCLOUD_AUTH" | base64 --decode | wc
+<"$HOME"/gcloud.json | wc
 sh -c "gcloud auth activate-service-account --key-file=$HOME/gcloud.json $*"
