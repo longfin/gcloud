c4ecf26005c0c934f418ac22096ce883ba41aefb
Chris Patterson <chrispat@github.com>
2019-12-02 15:13:56 -0500

    Added deprecation warning

--- auth/entrypoint.sh
+++ auth/entrypoint.sh
@@ -2,5 +2,7 @@
 
 set -e
 
+echo "##[warning] This actions has been deprecated in favor of https://github.com/GoogleCloudPlatform/github-actions.  This repo has been archived and will be made private on 12/31/2019"
+
 echo "$GCLOUD_AUTH" | base64 --decode > "$HOME"/gcloud.json
 sh -c "gcloud auth activate-service-account --key-file=$HOME/gcloud.json $*"
